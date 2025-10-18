2e3c21790dbf7294e77b89670e1e992500b9842a
manav-senpai <rockstarsigma@gmail.com>
2025-10-19 03:54:38 +0530

    done learning indexing

--- week2/day8/Indexing.py
+++ week2/day8/Indexing.py
@@ -1,1 +1,18 @@
-# indexing = accessing elements of sequence using []
+# indexing = accessing elements of sequence using [] (indexing operator ) [start : end : step ]
+from traceback import print_tb
+
+credit_number = "1234-2342-1234-6456-7653"
+
+# print(credit_number[0])
+# print(credit_number[0:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-1])
+# print(credit_number[::2])
+
+#create a program to get last 4 digits of the number
+
+credit_number_rev = credit_number[::-1]
+last_digits = credit_number[-4:]
+print(f"xxxx-xxxx-xxxx-{last_digits}")
+print(credit_number_rev)

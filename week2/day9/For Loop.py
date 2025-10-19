ed05257b6e111de306b086c238b6030e0d43912e
manav-senpai <rockstarsigma@gmail.com>
2025-10-19 21:36:44 +0530

    Done with while loop and for loop, so just doing a commit...glory to mundane

---
+++ week2/day9/For Loop.py
@@ -0,0 +1,18 @@
+# for loop = execute a block of code a fixed number of times.
+#           You can iterate over a range, string, sequence, etc.
+
+# for x in  range(1, 11):
+#     print(x)
+# for x in  reversed(range(1, 11, 2)):
+#     print(x)
+# credit_card = "1254-3453-5462-1233"
+# for x in credit_card:
+#     print(x)
+
+#CONTINUE AND BREAK ( CAN BE USED IN FOR LOOPS/ WHILE LOOPS ETC)
+
+for x in range(1,21):
+    if x == 13:
+        break
+    else:
+        print(x)

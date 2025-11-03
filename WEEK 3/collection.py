3dfd2b85cda34d99eeeba2e2812d3f66749043a9
manav-senpai <rockstarsigma@gmail.com>
2025-11-04 02:39:51 +0530

    created a new file today

--- WEEK 3/collection.py
+++ WEEK 3/collection.py
@@ -43,11 +43,14 @@
 fruits = ("apple", "orange", "banana", "coconut","cherry","cherry")
 # print(dir(fruits))
 
-print("Kaisi hai? kya kar rahi hai ")
-response = input("1.busy \n 2.tired \n3.tabiyat teek nahi \n4.bore ho raha hai \n5.Din bhar padhai kari, ab man nahi lag rha\n")
-if response == "1" or response == "2" or response == "3":
-        print("alr...koi na")
-elif response == "4":
-        print("game mein aaja phir")
-else:
-        print("game khel lete hai, mood fresh ho jayega")
+# A PROGRAM TO INVITE FRIREN FOR GAME
+
+# print("Kaisi hai? kya kar rahi hai ")
+# response = input("1.busy \n 2.tired \n3.tabiyat teek nahi \n4.bore ho raha hai \n5.Din bhar padhai kari, ab man nahi lag rha\n")
+# if response == "1" or response == "2" or response == "3":
+#         print("alr...koi na")
+# elif response == "4":
+#         print("game mein aaja phir")
+# else:
+#         print("game khel lete hai, mood fresh ho jayega")
+print("inject gallanium into the vein, dr cameron please process with the injection")

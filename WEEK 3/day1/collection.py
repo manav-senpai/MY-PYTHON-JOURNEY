577a1876928b93aa8db957ddad9c555684af9f75
manav-senpai <rockstarsigma@gmail.com>
2025-11-05 00:27:10 +0530

    alr..the changes made on 5th

--- WEEK 3/day1/collection.py
+++ WEEK 3/day1/collection.py
@@ -43,14 +43,8 @@
 fruits = ("apple", "orange", "banana", "coconut","cherry","cherry")
 # print(dir(fruits))
 
-# A PROGRAM TO INVITE FRIREN FOR GAME
-
-# print("Kaisi hai? kya kar rahi hai ")
-# response = input("1.busy \n 2.tired \n3.tabiyat teek nahi \n4.bore ho raha hai \n5.Din bhar padhai kari, ab man nahi lag rha\n")
-# if response == "1" or response == "2" or response == "3":
-#         print("alr...koi na")
-# elif response == "4":
-#         print("game mein aaja phir")
-# else:
-#         print("game khel lete hai, mood fresh ho jayega")
-print("inject gallanium into the vein, dr cameron please process with the injection")
+# print("inject gallanium into the vein, dr cameron please process with the injection")
+# print(fruits.index("apple"))
+# print(fruits.count("cherry"))
+# print(fruits.count("apple"))
+# print(fruits)
---
+++ WEEK 3/day2/InviteFriren.py
@@ -0,0 +1,10 @@
+# A PROGRAM TO INVITE FRIREN FOR GAME
+
+# print("Kaisi hai? kya kar rahi hai ")
+# response = input("1.busy \n 2.tired \n3.tabiyat teek nahi \n4.bore ho raha hai \n5.Din bhar padhai kari, ab man nahi lag rha\n")
+# if response == "1" or response == "2" or response == "3":
+#         print("alr...koi na")
+# elif response == "4":
+#         print("game mein aaja phir")
+# else:
+#         print("game khel lete hai, mood fresh ho jayega")
